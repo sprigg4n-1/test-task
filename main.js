adb7166e86df1633a7d2a9ecaceef27e20816bcf
Roman <sprigg4n.me@gmail.com>
2022-10-28 15:06:29 +0300

    First commit

---
+++ main.js
@@ -0,0 +1,44 @@
+const btnsNum = document.querySelectorAll(".btn-num");
+const input = document.querySelector(".input");
+const btnClear = document.querySelector(".btn-clear");
+const btnCencel = document.querySelector(".btn-cancel");
+const btnEnter = document.querySelector(".btn-enter");
+const btnHide = document.querySelector(".hide");
+const btnShow = document.querySelector(".show")
+
+btnsNum.forEach(item => {
+    item.addEventListener("click", () => {
+        input.value += item.innerHTML;
+    })
+})
+
+if(input.value.length > 4) {
+    input.value = input.value.slice(0, -1);
+}
+
+btnCencel.addEventListener("click", () => {
+    input.value = input.value.slice(0, -1);
+})
+
+btnClear.addEventListener("click", () => {
+    input.value = "";
+})
+
+btnEnter.addEventListener("click", () => {
+    alert(input.value)
+})
+
+btnHide.addEventListener("click", () => {
+    input.type = "password";
+
+})
+
+btnShow.addEventListener("click", () => {
+    input.type = "text";
+
+})
+
+
+
+
+
